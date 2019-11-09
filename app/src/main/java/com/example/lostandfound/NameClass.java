dc11d4715926ff6fdaa3a96e8b2cd78c631760d5
sayanmanik <sayanmanik.sm@gmail.com>
2019-11-09 21:04:05 +0530

    Name Class modified

--- app/src/main/java/com/example/lostandfound/NameClass.java
+++ app/src/main/java/com/example/lostandfound/NameClass.java
@@ -4,4 +4,7 @@ public class NameClass
 {
     public static String logIn = "LogIn";
     public static String signUp = "SignUp";
+
+    public static String nameForStoringDatabase = "name";
+    public static String radioButtonText = "category";
 }
