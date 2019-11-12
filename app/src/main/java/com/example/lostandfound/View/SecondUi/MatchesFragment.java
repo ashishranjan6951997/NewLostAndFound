b4925706d85d50e8b35f129d354c6265a160aaf6
sayanmanik <sayanmanik.sm@gmail.com>
2019-11-12 17:06:56 +0530

    adding a change from the final branch

--- app/src/main/java/com/example/lostandfound/View/SecondUi/MatchesFragment.java
+++ app/src/main/java/com/example/lostandfound/View/SecondUi/MatchesFragment.java
@@ -4,19 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.lostandfound.R;
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 
 public class MatchesFragment extends Fragment {
 
+    View rootView;
+    SwipeFlingAdapterView flingContainer;
+    TextView descText;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.matches_fragment,container,false);
+        rootView =  inflater.inflate(R.layout.matches_fragment,container,false);
+        init();
+        return rootView;
+    }
+
+    private void init()
+    {
+        flingContainer = rootView.findViewById(R.id.frame);
+        descText = rootView.findViewById(R.id.desc);
+
     }
 }
