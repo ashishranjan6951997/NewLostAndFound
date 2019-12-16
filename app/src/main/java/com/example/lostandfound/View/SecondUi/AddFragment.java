da570f9b990c79b6615d9ee10ed34a463a2b2bc3
sayanmanik <sayanmanik.sm@gmail.com>
2019-12-16 12:04:57 +0530

    matches fragment error corrected

--- app/src/main/java/com/example/lostandfound/View/SecondUi/AddFragment.java
+++ app/src/main/java/com/example/lostandfound/View/SecondUi/AddFragment.java
@@ -17,11 +17,10 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
-<<<<<<< HEAD
+
 import android.widget.TimePicker;
-=======
+
 import android.widget.Toast;
->>>>>>> 3542a9d50947bf49c9f31de257ea766d44a6a791
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
